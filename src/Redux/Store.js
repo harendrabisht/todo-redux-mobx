ae1f697143596bcc1df7454da1bb094c2c078a1e
harendra <harendra.bisht2@gmail.com>
2018-09-04 13:08:45 +0530

    thunk

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,5 +1,5 @@
 import {createStore, applyMiddleware} from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import {thunkMiddleware} from 'redux-thunk';
 import reducers from './Reducer';
 const middleWare = applyMiddleware(thunkMiddleware)(createStore);
 const store = middleWare(reducers);
